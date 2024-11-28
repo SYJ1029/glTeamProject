72d2b690b81b867c85a633a90618d04874d165b0
DESKTOP-HGMKATH\abg09 <abg0986@gmail.com>
2024-11-29 00:03:53 +0900

    Update GetRandomNumber(int seed)
    함수내용 약간 수정, 주석 추가

--- glTeamProject/glTeamProject/main.cpp
+++ glTeamProject/glTeamProject/main.cpp
@@ -490,7 +490,8 @@ void InitPlayer() {
 
 
 GLfloat GetRandomNumber(int seed) {
-	float result = (float)((float)rand() / RAND_MAX) * (seed * 11) - (seed * 5.5f);
+	// 결과값의 범위는 [-seed/2, seed/2]
+	float result = (float)((float)rand() / RAND_MAX) * seed - ((float)seed / 2.0f);
 
 	return result;
 }
